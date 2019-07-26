5728089c3c0755f0cefcbde201e2f5dcf5f3ebb6
Apple <apple@Apples-MacBook-Pro-66.local>
2019-07-26 09:59:08 -0400

    Complete Iteration 0

---
+++ Emojianal Project/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Emojianal Project
+//
+//  Created by Apple on 7/26/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+}
+
